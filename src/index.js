79050ca2626cc165601c266aba57ebf221422338
大ædamingerdai <mingguobin@live.com>
2020-08-25 23:22:00 +0800

    build: use scss instead of css

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import "./index.css";
+import "./index.scss";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,7 +27,7 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" >
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
