7b7fb63659d5715b4e0b4e7f01391cb9071eee4d
大ædamingerdai <mingguobin@live.com>
2020-09-21 08:51:38 +0800

    add greeting component

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Clock } from "./clock/clock";
+import { Greeting } from './greeting/Greeting';
 
 export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+  }
+
+  toggleIsLogin() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
   render() {
     return <div>
+        <button  className="btn btn-primary" onClick={() => this.toggleIsLogin()}>按钮</button>
+        <Greeting isLoggedIn={this.state.isToggleOn} />,
+
         <Clock></Clock>
         <Clock></Clock>
         <Clock></Clock>
---
+++ src/app/greeting/GuestGreeting.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function GuestGreeting(props) {
+    return <h1>Plase sign up!.</h1>
+}
