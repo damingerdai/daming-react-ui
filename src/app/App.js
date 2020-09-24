a51b3a439c086c390390eb45f1907b270843123c
大ædamingerdai <mingguobin@live.com>
2020-09-24 22:12:03 +0800

    feat: add number list

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Clock } from "./clock/clock";
 import { Greeting } from './greeting/Greeting';
+import { NumberList } from './list/numbers';
 
 export class App extends React.Component {
+
 
   constructor(props) {
     super(props);
@@ -15,10 +17,15 @@ export class App extends React.Component {
     }));
   }
 
+ 
+
   render() {
+    const numbers = [1, 2, 3, 4, 5]
     return <div>
         <button  className="btn btn-primary" onClick={() => this.toggleIsLogin()}>按钮</button>
         <Greeting isLoggedIn={this.state.isToggleOn} />,
+
+        <NumberList numbers={numbers}></NumberList>
 
         <Clock></Clock>
         <Clock></Clock>
