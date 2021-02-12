933e144129f32af5fac93f242e372a16a6a945f2
Arthur Ming <guobin.ming@pwc.com>
2021-02-12 21:22:46 +0800

    feat:  material的首页完成

--- src/app/App.js
+++ src/app/App.js
@@ -1,81 +1,39 @@
 import React from "react";
-import { Clock } from "./clock/clock";
-import { WelcomeDialog } from "./composition-vs-inheritance/welcome-dialog";
-import { Greeting } from "./greeting/Greeting";
-import { HooksExample } from "./hooks";
-import { Calculator } from "./lifting-state-up/Calculator";
-import { NumberList } from "./list/numbers";
-import { Blog } from "./blog/blog";
-import { NameForm } from "./forms/NameForm";
-import { EssayForm } from "./forms/EssayForm";
-import { FlavorForm } from "./forms/FlavorForm";
-import { Reservation } from "./forms/Reservation";
-import { ProductTable } from "./search/ProductTable";
-import Counter from './redux';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import "./App.scss";
 
-import './App.scss';
+export { store } from "./redux";
 
-export { store } from './redux';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
 
-class Hello extends React.Component {
-  render() {
-    return React.createElement("div", null, `Hello ${this.props.name}`);
-  }
-}
+export const App = () => {
+  const classes = useStyles();
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isToggleOn: true };
-  }
-
-  toggleIsLogin() {
-    this.setState((state) => ({
-      isToggleOn: !state.isToggleOn,
-    }));
-  }
-
-  render() {
-    const numbers = [1, 2, 3, 4, 5];
-    const posts = [
-      { id: 1, title: "Hello World", content: "Welcome to learning React!" },
-      {
-        id: 2,
-        title: "Installation",
-        content: "You can install React from npm.",
-      },
-    ];
-    return (
-      <div>
-        <nav className="navbar navbar-dark bg-primary">
-          <p className="navbar-brand">Navbar</p>
-        </nav>
-        <div className="content">
-          <Counter />
-          <HooksExample />
-          <ProductTable />
-          <Calculator />
-          <Hello name="damingerdai" />
-          <button
-            className="btn btn-primary"
-            onClick={() => this.toggleIsLogin()}
-          >
-            按钮
-          </button>
-          <Greeting isLoggedIn={this.state.isToggleOn} />,
-          <NumberList numbers={numbers}></NumberList>
-          <Blog posts={posts}></Blog>
-          <NameForm />
-          <EssayForm />
-          <FlavorForm />
-          <Reservation />
-          <Clock></Clock>
-          <Clock></Clock>
-          <Clock></Clock>
-          <Calculator />
-          <WelcomeDialog />
-        </div>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            大明
+          </Typography>
+          <div className="flex-space"></div>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
